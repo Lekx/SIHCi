501260a38f145b43145d32b022d71b38493d2669
ismael gonzalez <ismael.gonzalez@puntomedio.net>
2015-01-30 16:32:14 -0600

    Documentos subidos a github (OP01,OP05,OP01) -> Controlador y vista

---
+++ sihcig/protected/views/registroReniecyt/index.php
@@ -0,0 +1,32 @@
+
+
+<section>
+	<h1>Iconos!</h1>
+</section>
+
+<section>
+
+	Investigación
+
+		<p>El Organismo Público Descentralizado Hospital Civil de Guadalajara es el hospital- escuela de la Universidad de Guadalajara que forma el mayor número de médicos especialistas en México, un promedio de 220 especialistas por año. Nuestros médicos son reconocidos por el número de enfermos que diagnostican y curan; queremos que también lo sean, por la calidad del conocimiento científico que producen para la innovación en medicina, con beneficios evidentes, al ofrecer soluciones para los pacientes y la comunidad en general. Innovar significa utilizar el conocimiento de las investigaciones para mejorar los servicios de salud y las tecnologías. </p>
+
+		<p>El conocimiento científico, en todas las disciplinas de las ciencias de la salud, solamente tiene impacto social y económico cuando es aplicado para el beneficio de los enfermos y las comunidades, sin embargo, en México, el conocimiento científico útil de las investigaciones locales y nacionales frecuentemente es ignorado por años o décadas; según la Organización Mundial de la Salud “desestimar el conocimiento científico es nocivo para los individuos y las poblaciones, y representa un desperdicio de recursos”. </p>
+
+		<p>Existen una gran cantidad de proyectos de investigación en ciencias de la salud que ofrecen conocimiento científico novedoso, que tradicionalmente es presentado en congresos nacionales e internacionales, pero no se transfieren para su aplicación en programas de salud, programas educativos, registros de propiedad intelectual o políticas públicas. </p>
+
+		<p>“Llevar un conocimiento nuevo a la práctica clínica, es tan importante como el descubrimiento mismo”; por lo que, para lograr el desarrollo de la investigación en el OPD Hospital Civil de Guadalajara y formar profesionales de las ciencias de la salud que transformen el conocimiento científico con una visión ética y humanista para la innovación en México, se proponen 4 ejes temáticos:</p>
+	<UL>
+		<li>Regulación de los Procesos de la Investigación</li>
+	    <li>Gestión de Recursos para la Investigación</li>
+		<li>Generación de Conocimiento Científico</li>
+		<li>Aplicación de Conocimiento Científico para la Innovación</li>
+		
+	</UL>
+		
+</section>
+
+
+<section>
+	<h1>Documentos Descargables!</h1>
+</section>
+
