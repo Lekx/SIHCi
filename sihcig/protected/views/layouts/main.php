449614ad9b59e01c147141bb6001d03b307687a6
Daniel Ulises Garcia Verdin <udevelopd@gmail.com>
2015-02-03 13:16:11 -0600

    Realizando el Mapa del sitio.

--- sihcig/protected/views/layouts/main.php
+++ sihcig/protected/views/layouts/main.php
@@ -22,17 +22,23 @@
 
 <body>
 
-<?php Yii::app()->bootstrap->register(); ?>
+	<?php Yii::app()->bootstrap->register(); ?>
 
 	<section class="logosection">
 		<div class="logo"></div>
 		<div class="logosub"></div>
-		<div class="logoinfo"></div>
-		<div class="logonum"></div>
+		<div class="logoinfo"><span>Ivestigadores:</span><span>Vidas Cambiadas</span><span>Ivestigaciones</span><span>Libros Y Revistas</span></div>
+		<div class="logonum"><h4>1,456</h4><h4>1,923,456</h4><h4>17,296</h4><h4>3,163</h4></div>
 	</section>
 
 	<section class="logsection">
-		<div class="login"></div><div class="singin"></div><div class="searchbar"></div>
+		<div class="login"></div>
+		<div class="singin"></div>
+		<div class="searchbar">
+			<form>
+				<input type="text" placeholder="Buscar..">
+			</form>
+		</div>
 	</section>	
 
 	<section>
@@ -71,6 +77,108 @@
 
 	<section>
 		<div class="carrusel"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner.jpg"></div>
+	</section>
+
+	<section>
+
+		<div class="menu">
+			<ul class="cbp-hsmenu">
+				<div>
+					<li>
+						<a href="#">OPD HCG</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+							<li><a><span>Dirección general</span></a></li>
+							<li><a><span>Sudirección general de enseñanza e investigación</span></a></li>
+							<li><a><span>Organigrama</span></a></li>
+							<li><a><span>Normatividad de investigación</span></a></li>
+							<li><a><span>Registro RENIECYT</span></a></li>
+							<li><a><span>Tramites y servicios</span></a></li>
+							<li><a><span>Transparencia</span></a></li>
+							<li><a><span>FInEHC</span></a></li>
+							<li><a><span>Plano de ubicación</span></a></li>
+						</ul>
+					</li>
+
+					<li>
+						<a href="#">Programas de coopreación internacional en investigación</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Vinculación con universidades, institutos y hospitales</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+				</div>
+
+				<div>
+					<li>
+						<a href="#">Programas de generación de conocmiento cientifico</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Unidad Hospitalaria Juan I. Mecha</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Centro de investigación clinica medicina traslacional</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+				</div>
+
+				<div>
+					<li>
+						<a href="#">Unidades de investigación</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Laboratorios de investigación</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Unidad Hospitalaria Fray Antonio Alcalde</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+				</div>
+
+				<div>
+					<li>
+						<a href="#">Formación de recursos en investigación</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Convocatoria y Becas para Formar Recursos Humanos en invesitigación</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">Unidad Editorial</a>
+						<ul class="cbp-hssubmenu">
+							<li><a href="#"><span>Delicate Wine</span></a></li>
+						</ul>
+					</li>
+				</div>
+			</ul>
+		</div>
+
 	</section>
 
 
