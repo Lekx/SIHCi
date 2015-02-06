fb9d7274981834062b4cc0e8df5aa6e6f4a40514
Daniel Ulises Garcia Verdin <udevelopd@gmail.com>
2015-02-06 12:55:00 -0600

    SlideShow Terminado Y Font

--- sihcig/js/list.js
+++ sihcig/js/list.js
@@ -1,51 +1,40 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
 	// body...
-
-
 $('.cbp-hssubmenu1 > li').hide();
 
-$('.cbp-hsmenu1 > li').each(function () {
-    $(this).click(function () {
-        $('.cbp-hsmenu1 > li').not(this).find('li').slideUp();
-        $(this).find('.cbp-hssubmenu1 li').slideDown();
+ $(".slidingDiv").hover(function () {
+        $(this).find('.cbp-hsmenu1 li').slideDown();
         return false;
     });
-});
 
-$('.cbp-hssubmenu2 > li').hide();
-
-$('.cbp-hsmenu2 > li').each(function () {
-    $(this).click(function () {
-        $('.cbp-hsmenu2 > li').not(this).find('li').slideUp();
-        $(this).find('.cbp-hssubmenu2 li').slideDown();
+ $('.slidingDiv').mouseleave(function () {
+        $('.slidingDiv').slideUp();
         return false;
     });
+     
 });
 
-$('.cbp-hssubmenu3 > li').hide();
+$(document).ready(function () {
 
-$('.cbp-hsmenu3 > li').each(function () {
-    $(this).click(function () {
-        $('.cbp-hsmenu3 > li').not(this).find('li').slideUp();
-        $(this).find('.cbp-hssubmenu3 li').slideDown();
+ $('#show_hidemenu').click(function(){
+
+        $('.cbp-hssubmenu1 > li').hide();
+        $(".slidingDiv").slideToggle();
         return false;
     });
-});
-
-$('.cbp-hssubmenu4 > li').hide();
-
-$('.cbp-hsmenu4 > li').each(function () {
-    $(this).click(function () {
-        $('.cbp-hsmenu4 > li').not(this).find('li').slideUp();
-        $(this).find('.cbp-hssubmenu4 li').slideDown();
-        return false;
-    });
-});
-
-
-
 
 });
 
-*/
+
+
+ 
+    
+
+
+
+
+
+
+
+
 
--- sihcig/protected/views/layouts/main.php
+++ sihcig/protected/views/layouts/main.php
@@ -12,7 +12,7 @@
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
 	<![endif]-->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap-3.0.0/css/bootstrap.min.css" media="screen, projection" >
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/bootstrap-3.0.0/css/bootstrap.min.css" media="screen, projection">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css">
@@ -24,6 +24,8 @@
 	$baseUrl = Yii::app()->baseUrl; 
 	$cs = Yii::app()->getClientScript();
 	$cs->registerScriptFile($baseUrl.'/js/list.js');
+	$cs->registerScriptFile($baseUrl.'/js/prefixfree.min');
+	$cs->registerScriptFile($baseUrl.'/js/slideshow.js');
 	?>
 
 	<?php Yii::app()->bootstrap->register(); ?>
@@ -34,10 +36,109 @@
 
 <body>
 
+	<section>
+		<div class="slidingDiv">
+			<div class="menu">
+			<div>
+				<ul class="cbp-hsmenu1">
+					<li><a>OPD HCG</a>
+						<ul class="cbp-hssubmenu1">
+							<li><a><span>Delicate Wine</span></a></li>
+							<li><a><span>Dirección general</span></a></li>
+							<li><a><span>Sudirección general de enseñanza e investigación</span></a></li>
+							<li><a><span>Organigrama</span></a></li>
+							<li><a><span>Normatividad de investigación</span></a></li>
+							<li><a><span>Registro RENIECYT</span></a></li>
+							<li><a><span>Tramites y servicios</span></a></li>
+							<li><a><span>Transparencia</span></a></li>
+							<li><a><span>FInEHC</span></a></li>
+							<li><a><span>Plano de ubicación</span></a></li>
+						</ul>
+					</li>
+					<li>
+						<a>Programas de coopreación internacional en investigación</a>
+						<ul class="cbp-hssubmenu1">
+						</ul>
+					</li>
+
+					<li>
+						<a>Vinculación con universidades, institutos y hospitales</a>
+						<ul class="cbp-hssubmenu1">
+						</ul>
+					</li>
+					<ul>
+					</div>
+					<div>
+						<ul class="cbp-hsmenu1">
+							<li>
+								<a>Unidad Editorial</a>
+								<ul class="cbp-hssubmenu1">
+								</ul>
+							</li>
+							<li>
+								<a>Programas de generación de conocmiento cientifico</a>
+								<ul class="cbp-hssubmenu1">
+									<li><a><span>Redacción científica</span></a></li>
+									<li><a><span>Asesoría Metodológica</span></a></li>
+									<li><a><span>Asesoría de corrección de estilo de redacción en español u otros idiomas</span></a></li>
+									<li><a><span>Desplegar lineas de generación de conocimiento científico<span><a></li>
+								</ul>
+							</li>
+							<li>
+								<a>Programas de desarrollo tecnológico e innovación</a>
+								<ul class="cbp-hssubmenu1">
+									<li><a><span>proINVENCI</span></a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<ul class="cbp-hsmenu1">
+							<li>
+								<a>Centro de investigación clinica medicina traslacional</a>
+								<ul class="cbp-hssubmenu1">
+								</ul>
+							</li>
+							<li>
+								<a>Unidades de investigación</a>
+								<ul class="cbp-hssubmenu1">
+								</ul>
+							</li>
+							<li>
+								<a>Formación de recursos en investigación</a>
+								<ul class="cbp-hssubmenu1">
+									<li><a><span>Delicate Wine</span></a></li>
+									<li><a><span>Programas PNCP</span></a></li>
+									<li><a><span>Programas NO PNCP</span></a></li>
+									<li><a><span>Programas no medicos</span></a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+					<div>
+						<ul class="cbp-hsmenu1">
+							<li>
+								<a>Convocatoria y Becas para Formar Recursos Humanos en invesitigación</a>
+								<ul class="cbp-hssubmenu1">
+								</ul>
+							</li>
+							
+							<li>
+								<a>Unidad Editorial</a>
+								<ul class="cbp-hssubmenu1">
+								</ul>
+							</li>
+
+						</ul>
+					</div>	
+				</div>
+		</div>
+	</section>
+
 	<section class="logosection">
 		<div class="logo"></div>
 		<div class="logosub"></div>
-		<div class="logoinfo"><span>Ivestigadores:</span><span>Vidas Cambiadas</span><span>Ivestigaciones</span><span>Libros Y Revistas</span></div>
+		<div class="logoinfo"><span>Ivestigadores:</span><span>Vidas Cambiadas</span><span>Ivestigaciones</span><span>Libros y Revistas</span></div>
 		<div class="logonum"><h4>1,456</h4><h4>1,923,456</h4><h4>17,296</h4><h4>3,163</h4></div>
 	</section>
 
@@ -46,13 +147,46 @@
 		<div class="singin"></div>
 		<div class="searchbar">
 			<form>
-				<input type="text" placeholder="Buscar..">
-			</form>
+
+
+				<button id="show_hidemenu" type="button" class="btn btn-default btn-lg">
+				<span class="glyphicon glyphicon-align-justify"></span>
+				</button>
+				
+				
+				<input type="text" class="form-control" placeholder="Search" aria-describedby="basic-addon1">
+				<button id="search" type="button" class="btn btn-default btn-lg">
+					<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+				</button>
 		</div>
 	</section>	
 
 	<section>
-		<div class="carrusel"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner.jpg"></div>
+		<div class="slideshow">
+			<div class="inner">
+				<div class="img-block on">
+					<div class="img-wrap">
+						<div><img id="img1" src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner.jpg"></div>
+					</div>
+				</div>
+				<div class="img-block">
+					<div class="img-wrap">
+						<div><img id="img1" src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner2.jpg"></div>
+					</div>
+				</div>
+				<div class="img-block">
+					<div class="img-wrap">
+						<div><img id="img1" src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner3.jpg"></div>
+					</div>
+				</div>
+				<div class="img-block">
+					<div class="img-wrap">
+						<div><img id="img1" src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner4.jpg"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="carrusel"></div>
 	</section>
 
 	<section class="carruselinfo">
@@ -89,13 +223,13 @@
 		<div class="carrusel"><img src="<?php echo Yii::app()->request->baseUrl; ?>/img/banner.jpg"></div>
 	</section>
 
-	<section>
 
+	<section class="mapaSitio">
 		<div class="menu">
 			<div>
-				<ul class="cbp-hsmenu1">
+				<ul class="cbp-hsmenu">
 					<li><a>OPD HCG</a>
-						<ul class="cbp-hssubmenu1">
+						<ul class="cbp-hssubmenu">
 							<li><a><span>Delicate Wine</span></a></li>
 							<li><a><span>Dirección general</span></a></li>
 							<li><a><span>Sudirección general de enseñanza e investigación</span></a></li>
@@ -110,28 +244,27 @@
 					</li>
 					<li>
 						<a>Programas de coopreación internacional en investigación</a>
-						<ul class="cbp-hssubmenu1">
+						<ul class="cbp-hssubmenu">
 						</ul>
 					</li>
 
 					<li>
 						<a>Vinculación con universidades, institutos y hospitales</a>
-						<ul class="cbp-hssubmenu1">
+						<ul class="cbp-hssubmenu">
 						</ul>
 					</li>
 					<ul>
-			</div>
-
+					</div>
 					<div>
-						<ul class="cbp-hsmenu2">
+						<ul class="cbp-hsmenu">
 							<li>
 								<a>Unidad Editorial</a>
-								<ul class="cbp-hssubmenu2">
+								<ul class="cbp-hssubmenu">
 								</ul>
 							</li>
 							<li>
 								<a>Programas de generación de conocmiento cientifico</a>
-								<ul class="cbp-hssubmenu2">
+								<ul class="cbp-hssubmenu">
 									<li><a><span>Redacción científica</span></a></li>
 									<li><a><span>Asesoría Metodológica</span></a></li>
 									<li><a><span>Asesoría de corrección de estilo de redacción en español u otros idiomas</span></a></li>
@@ -140,28 +273,27 @@
 							</li>
 							<li>
 								<a>Programas de desarrollo tecnológico e innovación</a>
-								<ul class="cbp-hssubmenu2">
+								<ul class="cbp-hssubmenu">
 									<li><a><span>proINVENCI</span></a></li>
 								</ul>
 							</li>
 						</ul>
 					</div>
-
 					<div>
-						<ul class="cbp-hsmenu3">
+						<ul class="cbp-hsmenu">
 							<li>
 								<a>Centro de investigación clinica medicina traslacional</a>
-								<ul class="cbp-hssubmenu3">
+								<ul class="cbp-hssubmenu">
 								</ul>
 							</li>
 							<li>
 								<a>Unidades de investigación</a>
-								<ul class="cbp-hssubmenu3">
+								<ul class="cbp-hssubmenu">
 								</ul>
 							</li>
 							<li>
 								<a>Formación de recursos en investigación</a>
-								<ul class="cbp-hssubmenu3">
+								<ul class="cbp-hssubmenu">
 									<li><a><span>Delicate Wine</span></a></li>
 									<li><a><span>Programas PNCP</span></a></li>
 									<li><a><span>Programas NO PNCP</span></a></li>
@@ -171,25 +303,29 @@
 						</ul>
 					</div>
 					<div>
-						<ul class="cbp-hsmenu3">
+						<ul class="cbp-hsmenu">
 							<li>
 								<a>Convocatoria y Becas para Formar Recursos Humanos en invesitigación</a>
-								<ul class="cbp-hssubmenu4">
+								<ul class="cbp-hssubmenu">
 								</ul>
 							</li>
 							
 							<li>
 								<a>Unidad Editorial</a>
-								<ul class="cbp-hssubmenu4">
+								<ul class="cbp-hssubmenu">
 								</ul>
 							</li>
-						
+
 						</ul>
-					</div>
-				
-			</div>
-
-
+					</div>	
+				</div>
 			</section>
+			<section class="footersection">
+				<div class="copyrigths"></div>
+				<div class="loginfot"></div>
+				<div class="singinfot"></div>
+			</section>
+
+
 		</body>
 		</html>
