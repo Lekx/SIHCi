b52958fbff377eba75dda151f7d82c79bfaeb2f9
RickTC <root@localhost.localdomain>
2015-03-06 13:25:44 -0600

    Tarea desplegar vinculacion con univesidad, institutos y hospitales terminada

---
+++ sihci/protected/views/vinculacionConUniversidadInstitutosHospitales/index.php
@@ -0,0 +1,41 @@
+VI01-Vinculacion Con Universidad Institutos Y Hospitales
+
+<section>
+	<h3>Icono</h3>
+
+</section>
+<section>
+<p>
+	<h6>Extensión y Vinculación</h6>
+	Las actividades de vinculación constituyen el puent
+	e entre los procesos de formación de recursos human
+	os
+	especializados y las instituciones de servicio y de
+	educación que potencialmente representan una forta
+	leza para
+	mejorar y coadyuvar en la educación de internos y e
+	specialistas en las unidades hospitalarias del OPD-
+	Hospital Civil
+	de Guadalajara. De igual manera, es la extensión el
+	mecanismo por el cual se puede realizar un acercam
+	iento entre
+	los procesos de enseñanza, investigación y servicio
+	, pero también entre estas y la comunidad hospitala
+	ria y la
+	sociedad local y regional en la cual se enclava el
+	Organismo Público Descentralizad
+
+</p>
+</section>
+
+<section class="column-right">
+	<h5>Programas y apoyos internacionales –Diciembre 2014</h5>
+	<h5>Convenios vigentes agosto 2014</h5>
+	<h5>Requisitos para la elaboración de convenios</h5>
+	<h5>Convenios internacionales junio 2014 </h5>	
+     	<?php Yii::app()->runController('filesManager/DisplayFiles/section/vinculacionConUniversidadInstitutosHospitales'); ?>
+ </section>
+
+
+
+
