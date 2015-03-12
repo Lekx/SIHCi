ccb2fa7d72aa48b19e4a0e81116c7df0dbcd21a7
Daniel Ulises Garcia Verdin <udevelopd@gmail.com>
2015-03-12 13:11:55 -0600

    layout arreglado

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -1,16 +1,12 @@
 <?php
 
-<<<<<<< HEAD
+
 // This is the database connection configuration.
 return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 
 	// uncomment the following lines to use a MySQL database
-=======
-
-return array(
 	
->>>>>>> a75acf37dbfd72078f867f22f688796332e3b534
 	'connectionString' => 'mysql:host=127.0.0.1;dbname=sihci',
 	'emulatePrepare' => true,
 	'username' => 'root',
