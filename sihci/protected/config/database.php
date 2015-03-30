0f6e543ff9e4fd8eac9d2150a5ff4e14b7b70d99
JoelValdivia <jooelvaldivi@gmail.com>
2015-03-30 11:56:54 -0600

    database config

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -4,17 +4,6 @@ return array(
 
 
 	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-<<<<<<< HEAD
-
-
-	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-
-	// uncomment the following lines to use a MySQL database
-
-
-	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-=======
->>>>>>> 5cc80df75dedb18c06950757cd35817f0b841d39
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '000',
