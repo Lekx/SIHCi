5cc80df75dedb18c06950757cd35817f0b841d39
Joel Valdivia <jooelvaldivi@gmail.com>
2015-03-30 11:50:47 -0600

    persons

--- sihci/protected/controllers/PersonsController.php
+++ sihci/protected/controllers/PersonsController.php
@@ -161,8 +161,6 @@ class PersonsController extends Controller
 							}
 
 				}else{
-
-<<<<<<< HEAD
 					$model->photo_url = YiiBase::getPathOfAlias("webroot").'/users/'.Yii::app()->user->id.'/cve-hc/perfil.png';
 						if($model->save()){
 							$curriculum->native_country = $curriculum->native_country;
@@ -173,9 +171,6 @@ class PersonsController extends Controller
 			}// end if validate
 		}// end isset $_POST
 
-
-=======
->>>>>>> 64f892da4da3c1d29a30c75085d7a7e4589e2591
 		$this->render('update',array('model'=>$model, 'curriculum'=>$curriculum));
 	}
 
