9bf33fedcb17aaab86bdec8835f501aad0ed5bd3
JoelValdivia <jooelvaldivi@gmail.com>
2015-04-21 10:43:33 -0500

    curriculum

--- sihci/protected/views/curriculumVitae/_form_research_areas.php
+++ sihci/protected/views/curriculumVitae/_form_research_areas.php
@@ -73,38 +73,34 @@
 	
 
 	<?php echo $form->errorSummary($model); ?>
-<input type="button" id="showForm" value="Agregar Documento">
-<input class="research" type="button" id="hideForm" value="Cancelar">
+<input type="button" id="showForm" value="Agregar Línea de Investigación">
+<input class="research"  type="button" id="hideForm" value="Cancelar">
 
 <div class="research">
+	<div class='row'>
 		Nombre de Investigación
-		<input id="research" type="text" name="nameResearch">
+		<input id="research" type="text" name="nameResearch" title="Nombre de Investigación" placeholder="Nombre de Investigación">
 		<div id="errorResearch" class="errors"> No debe ser nulo</div><br>
-		<input type="submit" id="btnCreate" value="Crear Línea de Investigación">
+		<input class="savebutton" type="submit" id="btnCreate" value="Crear Línea de Investigación">
+	</div>
 </div><!-- form -->
 <br>
 <br>
 	<?php 
 	$countDocs = 1;
 	foreach ($getResearch as $key => $value) {
-	
+		echo "<hr>";
+		echo "<div class='row'>";
 		echo "Linea de Investigacion ".$countDocs." ";
-		echo $form->textField($model,'name',array('name'=>'getResearch[]','value'=>$getResearch[$key]->name,'size'=>60,'maxlength'=>150, 'placeholder'=>'Nombre de investigación')); 
+		echo $form->textField($model,'name',array('title'=>'Nombre de Investigación','name'=>'getResearch[]','value'=>$getResearch[$key]->name,'size'=>60,'maxlength'=>150, 'placeholder'=>'Nombre de investigación')); 
 		echo $form->error($model,'name'); 
 		echo CHtml::button('Elminar',array('submit' => array('curriculumVitae/deleteResearch', 'id'=>$getResearch[$key]->id),'confirm'=>'¿Seguro que desea eliminarlo?'));
-		echo "<br>";
-		echo "------------------------------------------------------------";
-		echo "<br>";
+		echo "</div>";
+		echo "<hr>";
 		
 		$countDocs ++;
 	}
 	?>
-
-
-	<div class="row">
-		<?php echo $form->textField($model,'name',array( 'title'=>'Nombre de Investigación','size'=>60,'maxlength'=>150, 'placeholder'=>'Nombre de investigación')); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
 
 	<div class="row buttons">
 		<input class="savebutton" type="submit" onclick="validationFrom()" value="Guardar">
