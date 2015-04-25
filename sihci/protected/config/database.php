7a721b3257a5acf0c1e010b7da63d8e54c69af93
csaldana <jonathan.csaldana@alumno.udg.mx>
2015-04-24 22:10:00 -0500

    merge

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -4,10 +4,8 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-<<<<<<< HEAD
-	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-	//'connectionString' => 'mysql:host=localhost;dbname=sihci',
-=======
+	//'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
+	'connectionString' => 'mysql:host=localhost;dbname=sihci',
 
 	//'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
 	//'connectionString' => 'mysql:host=127.0.0.1;dbname=sihci',
@@ -15,14 +13,9 @@ return array(
 	//'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
 	//'connectionString' => 'mysql:host=127.0.0.1;dbname=sgeiadmi_sihci',
 
-
-	//'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-	//'connectionString' => 'mysql:host=localhost;dbname=sihci',
-
->>>>>>> 470371144e0442f07ddfe77504df3da9002f042b
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '000',
+	'password' => '',
 	'charset' => 'utf8',
 
 );
