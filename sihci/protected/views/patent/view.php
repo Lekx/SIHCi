4b7845297930fb95c20e616107808dca2ee614b9
Daniel Ulises Garcia Verdin <udevelopd@gmail.com>
2015-06-05 14:23:30 -0500

    Menu

--- sihci/protected/views/patent/view.php
+++ sihci/protected/views/patent/view.php
@@ -8,11 +8,21 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'Gestionar', 'url'=>array('admin')),
-	array('label'=>'Crear', 'url'=>array('create')),
-	//array('label'=>'Desplegar', 'url'=>array('index')),
-	//array('label'=>'Modificar', 'url'=>array('update', 'id'=>$model->id)),
-	//array('label'=>'Eliminar', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'¿Esta usted seguro de eliminar este registro?')),
+	//array('label'=>'List BooksChapters', 'url'=>array('index')),
+	array('label'=>'Graduados de posgrado ', 'url'=>array('admin')),
+	array('label'=>'Difusión de prensa ', 'url'=>array('admin')),
+	array('label'=>'Aplicacion de conocimiento ', 'url'=>array('admin')),
+	array('label'=>'Resgirtro patente ', 'url'=>array('admin')),
+	array('label'=>'Resgirtro derecho de autor', 'url'=>array('admin')),
+	array('label'=>'Resgirtro software', 'url'=>array('admin')),
+	array('label'=>'Articulos y guías', 'url'=>array('admin')),
+	array('label'=>'Libros ', 'url'=>array('admin')),
+	array('label'=>'Capítulo de libros ', 'url'=>array('admin')),
+	array('label'=>'Participación en congresos ', 'url'=>array('admin')),
+	array('label'=>'Tesis Dirigidas ', 'url'=>array('admin')),
+	array('label'=>'Certificaciones por consejos ', 'url'=>array('admin')),
+	//array('label'=>'View BooksChapters', 'url'=>array('view', 'id'=>$model->id)),
+	
 );
 ?>
 <div class="cvtitle">
