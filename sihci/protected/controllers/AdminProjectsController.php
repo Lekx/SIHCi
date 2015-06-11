e5f09450ab3783e871fa4bc0053b25b13dea2ecd
JoelValdivia <jooelvaldivi@gmail.com>
2015-06-11 13:20:49 -0500

    controlador

--- sihci/protected/controllers/AdminProjectsController.php
+++ sihci/protected/controllers/AdminProjectsController.php
@@ -28,7 +28,7 @@ class AdminProjectsController extends Controller {
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 			'actions'=>array('createProject', 'createSponsorship', 
 							 'update', 'deleteProject', 'view', 'index',
-							 'adminProjects'),
+							 'adminProjects', 'getSponsors'),
 			'users'=>array('*'),
 			),
 			array('deny',  // deny all users
@@ -63,12 +63,12 @@ class AdminProjectsController extends Controller {
 		{
 			$model->attributes=$_POST['Sponsorship'];
 			// $model->id_user_sponsorer = Yii::app()->user->id;
-			$model->status = "pendiente";
+			// $model->status = "pendiente";
 			if($model->save())
 				$this->redirect(array('adminProjects'));
 		}
 
-		$this->render('create_sponsorship', array('model' => $model));
+		$this->render('form_sponsorship', array('model' => $model));
 	}
 
 	/**
@@ -76,10 +76,31 @@ class AdminProjectsController extends Controller {
 	 * If update is successful, the browser will be redirected to the 'view' page.
 	 * @param integer $id the ID of the model to be updated
 	 */
-	public function actionUpdate($id) {
-			
+	public function actionUpdate($id, $folio) {
 
-		$this->render($update, array('id'=>$id,));
+		if ($folio != null) {
+			$model = Projects::model()->findByPk($id);
+
+			if (isset($_POST['Projects'])) {
+				$model->attributes = $_POST['Projects'];
+				if ($model->save()) {
+					$this->redirect(array('adminProjects'));
+				}
+			}
+			$update="update_projects";
+		}else{
+			$model = Sponsorship::model()->findByPk($id);
+
+			if (isset($_POST['Sponsorship'])) {
+				$model->attributes = $_POST['Sponsorship'];
+				if ($model->save()) {
+					$this->redirect(array('adminProjects'));
+				}
+			}
+			$update = "form_sponsorship";
+		}
+
+		$this->render($update, array('model'=>$model,));
 	}
 
 	/**
@@ -194,11 +215,10 @@ class AdminProjectsController extends Controller {
 
 	public function actionGetSponsors() {
 		if (Yii::app()->request->isAjaxRequest&&!empty($_GET['term'])) {
-			$sql = 'SELECT sp.* , s.sponsor_name 
-			FROM sponsorship sp 
-			JOIN users u ON u.id=sp.id_user_sponsorer
+			$sql = 'SELECT u.id, s.sponsor_name AS label
+			FROM users u 
 			JOIN sponsors s ON s.id_user = u.id
-			WHERE u.type="moral" AND u.status"activo" AND s.sponsor_name LIKE :qterm ORDER BY s.sponsor_name ASC';
+			WHERE u.type="moral" AND u.status="activo" AND s.sponsor_name LIKE :qterm ORDER BY s.sponsor_name ASC';
 			$command = Yii::app()->db->createCommand($sql);
 			$qterm = $_GET['term'].'%';
 			$command->bindParam(":qterm", $qterm, PDO::PARAM_STR);
