61a77fa93952ad3615767148acb848feb1c97e6e
Dan <udevelopd@gmail.com>
2015-06-16 18:26:53 -0500

    Incidencias Graduados y Research Areas y Specialty

--- sihci/protected/views/software/_search.php
+++ sihci/protected/views/software/_search.php
@@ -15,9 +15,9 @@
 	'method'=>'get',
 )); ?>
 		<div class="inner-addon right-addon">
-					 <i class="glyphicon glyphicon-search"></i>
+					 
 	   	<?php echo $form->textField($model,'searchValue',array('size'=>60,'maxlength'=>70, 'placeholder'=>'Ejemplo: Ricardo','class'=>'searchcrud')); ?>	
-		<?php //echo CHtml::submitButton('',array('class'=>'searchcrudbut')); ?>
+ 	<?php echo CHtml::submitButton('',array('class'=>'adminbut')); ?>
 
 	</div>
 	 
