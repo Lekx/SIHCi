b2efcc94cbd371e28bc998d403455dca51130070
JoelValdivia <jooelvaldivi@gmail.com>
2015-06-16 14:48:22 -0500

    bitácora al cerrar sesión

--- sihci/protected/controllers/SiteController.php
+++ sihci/protected/controllers/SiteController.php
@@ -126,7 +126,10 @@ class SiteController extends Controller {
 
 	// LO02 – Cerrar sesión
 	public function actionLogout() {
-
+				$section = "Logout";
+				$details = "Usuario: ".Yii::app()->user->fullname;
+				$action = "Cerró Sesión";
+				Yii::app()->runController('adminSystemLog/saveLog/section/'.$section.'/details/'.$details.'/action/'.$action);
 		Yii::app()->user->logout();
 		$this->redirect(Yii::app()->homeUrl);
 	}
