e5afff64c8bd4ca2761f5eeb2f414df42e0094c8
csaldana <jonathan.csaldana@alumno.udg.mx>
2015-06-18 14:56:00 -0500

    GR06-corregida

--- sihci/protected/views/charts/index.php
+++ sihci/protected/views/charts/index.php
@@ -5,7 +5,7 @@ $this->menu=array(
 	array('label'=>'Total de Proyectos Registrados', 'url'=>array('Charts/projectsTotal')),
 	array('label'=>'Total de libros Registrados', 'url'=>array('Charts/booksTotal')),
 	array('label'=>'Total de Capitulos de libros Registrados', 'url'=>array('Charts/chaptersTotal')),
-	array('label'=>'Total de Articulos y guias Registrados', 'url'=>array('Charts/articlesGuides')),
+	array('label'=>'Total de Articulos y Guías Registrados', 'url'=>array('Charts/articlesGuides_')),
 );
 require_once($action.".php");
  ?>
