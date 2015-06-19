99752d8651bd95c54940e077e5ee44542d798b25
Dan <udevelopd@gmail.com>
2015-06-19 15:42:37 -0500

    06/19/15

--- sihci/protected/views/sponsorship/update.php
+++ sihci/protected/views/sponsorship/update.php
@@ -9,13 +9,24 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'List Sponsorship', 'url'=>array('index')),
-	array('label'=>'Create Sponsorship', 'url'=>array('create')),
-	array('label'=>'View Sponsorship', 'url'=>array('view', 'id'=>$model->id)),
-	array('label'=>'Manage Sponsorship', 'url'=>array('admin')),
-);
-?>
+	//array('label'=>'List BooksChapters', 'url'=>array('indeºx')),
+	//array('label'=>'Evaluación CV', 'url'=>array('EvaluateCV/index')),
+	array('label'=>'Patrocinios', 'url'=>array('sponsorship/admin'),'itemOptions'=>array('class' => 'menuitem 1 now')),
+		array('label'=>'Crear', 'url'=>array('sponsorship/create'),'itemOptions'=>array('class' => 'sub')),
+		array('label'=>'Gestionar', 'url'=>array('sponsorship/admin'),'itemOptions'=>array('class' => 'sub')),
 
-<h1>Update Sponsorship <?php echo $model->id; ?></h1>
+
+	//array('label'=>'View BooksChapters', 'url'=>array('view', 'id'=>$model->id)),
+	
+	);
+?>
+<div class="cvtitle">
+            <img id=""src="<?php echo Yii::app()->request->baseUrl; ?>/img/icons/IconCirculo/ProgramasDesarrolloTecnologico.png" alt="">
+            <h1>Proyectos</h1>
+            <hr>
+        </div>
+
+<h3>Modificar de patrocinio: <?php echo $model->project_name; ?></h3>
+
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
