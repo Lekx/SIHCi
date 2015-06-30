33a1467605c728284256582317a3ccb45638ccdf
Dan <udevelopd@gmail.com>
2015-06-30 12:19:28 -0500

    FUCK .DS_strore

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -5,10 +5,7 @@ return array(
 
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-
-	//'connectionString' => 'mysql:host=localhost;dbname=sgeiadmi_sihci',
 	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '000',
