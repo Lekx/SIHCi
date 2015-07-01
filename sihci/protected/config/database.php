20e81127b14741754f1bad77e3b4c56ebe5a5ad9
JoelValdivia <jooelvaldivi@gmail.com>
2015-07-01 12:22:09 -0500

    form phones

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -5,13 +5,15 @@ return array(
 
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-
 	'connectionString' => 'mysql:host=localhost;dbname=sgeiadmi_sihci',
+	// 'connectionString' => 'mysql:host=localhost;dbname=sihci',
 	//'connectionString' => 'mysql:host=127.0.0.1;dbname=sihci',
 
 	'emulatePrepare' => true,
+	// 'username' => 'root',
 	'username' => 'sgeiadmi_sihci',
-	'password' => '51HC15Y573M',
+-	'password' => '51HC15Y573M',
+	// 'password' => '',
 	'charset' => 'utf8',
 );
 
