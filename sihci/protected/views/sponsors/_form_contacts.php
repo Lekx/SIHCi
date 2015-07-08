03152d2241ca44082d525224f419e881b2f9e9c9
ismael gonzalez coronado <mayelgonzalez@MacBook-Pro-de-ismael.local>
2015-07-08 14:32:40 -0500

    ajax sponsors 100%

--- sihci/protected/views/sponsors/_form_contacts.php
+++ sihci/protected/views/sponsors/_form_contacts.php
@@ -57,8 +57,11 @@ foreach ($fullname as $value) {
 
 
 <div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Crear' : 'Guardar',  array('class'=>'savebutton'));?>
-		<?php echo CHtml::Button('Cancelar',array('submit' => array('sponsors/sponsorsInfo'),'confirm'=>'¿Seguro que desea Cancelar?')); ?>
+	<?php echo CHtml::htmlButton('Enviar',array(
+							'onclick'=>'send("sponsors-contacts-form", "sponsors/create_contacts", "'.(isset($_GET['id']) ? $_GET['id'] : 0).'","'.Yii::app()->controller->id.'/'.Yii::app()->controller->action->id.'/'.(isset($_GET['id']) ? $_GET['id'] : 0).'","")',
+							'class'=>'savebutton',
+					));
+	?>		<?php echo CHtml::Button('Cancelar',array('submit' => array('sponsors/sponsorsInfo'),'confirm'=>'¿Seguro que desea Cancelar?')); ?>
 	</div>
 
 
