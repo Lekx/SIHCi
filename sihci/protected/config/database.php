00f34f38cd4781a74acbd5ee7f6326f2f2e59294
JoelValdivia <jooelvaldivi@gmail.com>
2015-07-17 10:51:29 -0500

    merge

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -3,10 +3,10 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 	//'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
-	'connectionString' => 'mysql:host=localhost;dbname=sgeiadmi_sihci',
+	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
 	'emulatePrepare' => true,
-	'username' => 'sgeiadmi_sihci',
-	'password' => '51HC15Y573M',
+	'username' => 'root',
+	'password' => '000',
 	'charset' => 'utf8',
 );
 ?>
