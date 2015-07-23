c512f81ce5ef048ee9cfc9c01314ed02a6c4d5ea
JoelValdivia <jooelvaldivi@gmail.com>
2015-07-23 14:30:51 -0500

    Orden descendente de los años en cada tabla

--- sihci/protected/controllers/TablesController.php
+++ sihci/protected/controllers/TablesController.php
@@ -96,7 +96,7 @@ class TablesController extends Controller
 	public function actionProjects()
 	{
 		$titlePage = "Proyectos de Investigación";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM projects')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM projects ORDER BY year DESC')->queryAll();
 		$projects=Yii::app()->db->createCommand('SELECT id FROM projects')->queryAll();
 
 		$query='SELECT u.id,p.names,pro.title, pro.discipline, pro.develop_uh, pro.is_sponsored, pro.registration_number, pro.status, pro.creation_date
@@ -117,7 +117,7 @@ class TablesController extends Controller
 	public function actionBooks()
 	{
 		$titlePage = "Libros";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM books')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM books ORDER BY year DESC')->queryAll();
 		$books=Yii::app()->db->createCommand('SELECT id FROM books')->queryAll();
 		$query='SELECT u.id,p.names,bo.book_title, bo.publisher, bo.release_date,bo.path, j.hospital_unit, bo.creation_date FROM books bo
 				 JOIN curriculum curri ON bo.id_curriculum=curri.id
@@ -137,7 +137,7 @@ class TablesController extends Controller
 	public function actionChapters()
 	{
 		$titlePage = "Capítulos de Libros";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM books_chapters')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM books_chapters ORDER BY year DESC')->queryAll();
 		$booksChapters=Yii::app()->db->createCommand('SELECT id FROM books_chapters')->queryAll();
 		$query='SELECT u.id,p.names,cha.chapter_title, cha.book_title, cha.publishers, cha.url_doc, j.hospital_unit, cha.creation_date FROM books_chapters cha
 				 JOIN curriculum curri ON cha.id_curriculum=curri.id
@@ -157,7 +157,7 @@ class TablesController extends Controller
 	public function actionPatents()
 	{
 		$titlePage = "Registro de Propiedad Intelectual: Patentes";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM patent')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM patent ORDER BY year DESC')->queryAll();
 		$patent=Yii::app()->db->createCommand('SELECT id FROM patent')->queryAll();
 		$query='SELECT u.id,p.names,pa.country, pa.name, pa.application_type, pa.application_number, pa.patent_type, j.hospital_unit, pa.creation_date
 				FROM patent pa
@@ -178,7 +178,7 @@ class TablesController extends Controller
 	public function actionSoftware()
 	{
 		$titlePage = "Registro de Propiedad Intelectual: Software";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM software')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM software ORDER BY year DESC')->queryAll();
 		$software=Yii::app()->db->createCommand('SELECT id FROM software')->queryAll();
 		$query='SELECT u.id,p.names, so.country, so.title, so.sector, so.organization, so.objective, so.path, j.hospital_unit, so.creation_date
 				FROM software so
@@ -199,7 +199,7 @@ class TablesController extends Controller
 	public function actionCopyrights()
 	{
 		$titlePage = "Registro de Propiedad Intelectual: Derechos de Autor";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM copyrights')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM copyrights ORDER BY year DESC')->queryAll();
 		$copyrights=Yii::app()->db->createCommand('SELECT id FROM copyrights')->queryAll();
 		$query='SELECT u.id,p.names, copy.participation_type, copy.title, copy.step_number, copy.application_date, j.hospital_unit, copy.creation_date
 				FROM copyrights copy
@@ -220,7 +220,7 @@ class TablesController extends Controller
 	public function actionArticlesGuides()
 	{
 		$titlePage = "Artículos y Guías";
-		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM articles_guides')->queryAll();
+		$year=Yii::app()->db->createCommand('SELECT DISTINCT YEAR(creation_date) as year FROM articles_guides ORDER BY year DESC')->queryAll();
 		$articles=Yii::app()->db->createCommand('SELECT id FROM articles_guides')->queryAll();
 		$query='SELECT u.id,p.names, ar.title, ar.article_type, ar.magazine, ar.url_document, j.hospital_unit, ar.creation_date
 				FROM articles_guides ar
