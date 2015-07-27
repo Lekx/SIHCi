06bcee8c9eee04d28f376c40798fcb091095ef3d
JoelValdivia <jooelvaldivi@gmail.com>
2015-07-27 11:20:37 -0500

    database

--- sihci/protected/config/database.php
+++ sihci/protected/config/database.php
@@ -5,7 +5,7 @@ return array(
 
 //	'connectionString' => 'mysql:host=192.168.1.26;dbname=sihci',
 	//'connectionString' => 'mysql:host=localhost;dbname=sgeiadmi_sihci',
-	'connectionString' => 'mysql:host=127.0.0.1;dbname=sihci',
+	'connectionString' => 'mysql:host=192.168.0.15;dbname=sihci',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '000',
