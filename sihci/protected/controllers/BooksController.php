c0a35e2e9aaa0a877a99f5bb835986fc1b4eee11
RickTC <ricardo.tapia@alumno.udg.mx>
2015-07-29 15:16:54 -0500

    cambie el como guarda el archivo en articulos y guias y solucion de borrar en libros

--- sihci/protected/controllers/BooksController.php
+++ sihci/protected/controllers/BooksController.php
@@ -243,12 +243,10 @@ class BooksController extends Controller
 		}
 		else
 			$this->loadModel($id)->delete();
-
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 			if(!isset($_GET['ajax']))
 				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 	}
-
 	public function actionDeleteAuthor($id, $idBooks){
 
 		$modelAuthors= BooksAuthors::model()->findByPk($id);
